10b52587e356356c98e856731d403ef73441a442
yusufyusie <yesuf023@gmail.com>
2024-02-13 01:08:34 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
+# config/routes.rb
 Rails.application.routes.draw do
   devise_for :users
-   # Root route
-   root 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # Home route
+  root to: 'home#index'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # User registration routes
+  resources :users, only: [:new, :create], path_names: { new: 'signup' }
+
+  # Session management routes
+  resource :session, only: [:new, :create, :destroy], path_names: { new: 'login', create: 'login', destroy: 'logout' }
+
+  # Health check route
+  get 'up', to: 'rails/health#show', as: :rails_health_check
+
+  # Other custom routes can go here
+  # ...
 end
