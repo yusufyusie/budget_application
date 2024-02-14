1a41918b706663cf33a397dc9cdfc1af4a60f675
yusufyusie <yesuf023@gmail.com>
2024-02-14 23:30:58 +0300

    Remove duplicate user

--- db/schema.rb
+++ db/schema.rb
@@ -41,26 +41,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_14_165749) do
     t.index ["author_id"], name: "index_purchases_on_author_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
   add_foreign_key "group_purchases", "groups", on_delete: :cascade
   add_foreign_key "group_purchases", "purchases", on_delete: :cascade
-  add_foreign_key "groups", "users", on_delete: :cascade
-  add_foreign_key "purchases", "users", column: "author_id", on_delete: :cascade
 end
