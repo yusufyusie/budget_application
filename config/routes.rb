544d7b660de3988d20fa239b43a66e47ccb47751
yusufyusie <yesuf023@gmail.com>
2024-02-14 21:19:55 +0300

    Add or enerate a group controller with an index action in Rails, you can use the rails generate controller command

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # config/routes.rb
 Rails.application.routes.draw do
+  get 'groups/index'
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
 
   # Dashboard route
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_14_061512) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_14_165749) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "group_purchases", force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "purchase_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_group_purchases_on_group_id"
+    t.index ["purchase_id"], name: "index_group_purchases_on_purchase_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
+  create_table "purchases", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_purchases_on_author_id"
+  end
+
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
@@ -32,4 +59,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_14_061512) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "group_purchases", "groups", on_delete: :cascade
+  add_foreign_key "group_purchases", "purchases", on_delete: :cascade
+  add_foreign_key "groups", "users", on_delete: :cascade
+  add_foreign_key "purchases", "users", column: "author_id", on_delete: :cascade
 end
