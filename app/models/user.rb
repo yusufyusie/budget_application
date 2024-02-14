71aacd958ba11dd26f7c281cb03fa5c8963d03e4
yusufyusie <yesuf023@gmail.com>
2024-02-14 07:52:04 +0300

    Add user migration with name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable, :confirmable
+
+has_many :purchases, foreign_key: 'author_id', dependent: :destroy
+has_many :groups, dependent: :destroy
+
+validates :name, presence: true, length: { maximum: 35 }
 end
