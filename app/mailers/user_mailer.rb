56737ab4c47205a8a1e91d1b7a4b910785d9aa3a
yusufyusie <yesuf023@gmail.com>
2024-02-14 13:50:07 +0300

    Add confirmation instraction view

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,11 @@ class UserMailer < ApplicationMailer
     @url  = new_user_session_url
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
+
+  def confirmation_instructions(user, token, opts={})
+    @user = user
+    @token = token
+    @url  = user_confirmation_url(@token)
+    mail(to: @user.email, subject: 'Confirmation instructions')
+  end
 end
