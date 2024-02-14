31e723d26fe47749a0e07946422164a64363f4ec
yusufyusie <yesuf023@gmail.com>
2024-02-14 23:44:44 +0300

    Add user migration only

--- db/schema.rb
+++ db/schema.rb
@@ -10,37 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_14_165749) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_14_061512) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "group_purchases", force: :cascade do |t|
-    t.bigint "group_id", null: false
-    t.bigint "purchase_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["group_id"], name: "index_group_purchases_on_group_id"
-    t.index ["purchase_id"], name: "index_group_purchases_on_purchase_id"
-  end
-
-  create_table "groups", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.string "name"
-    t.string "icon"
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_groups_on_user_id"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "purchases", force: :cascade do |t|
-    t.string "name"
-    t.decimal "amount"
-    t.bigint "author_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_purchases_on_author_id"
-  end
-
-  add_foreign_key "group_purchases", "groups", on_delete: :cascade
-  add_foreign_key "group_purchases", "purchases", on_delete: :cascade
 end
