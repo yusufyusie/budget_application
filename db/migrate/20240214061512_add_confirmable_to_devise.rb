1129171c81bef9b38eb6be6ef7dd72806042bff6
yusufyusie <yesuf023@gmail.com>
2024-02-14 20:51:56 +0300

    modify your migration

--- db/migrate/20240214061512_add_confirmable_to_devise.rb
+++ db/migrate/20240214061512_add_confirmable_to_devise.rb
@@ -5,10 +5,11 @@ class AddConfirmableToDevise < ActiveRecord::Migration[7.1]
     add_column :users, :confirmation_sent_at, :datetime
     add_column :users, :unconfirmed_email, :string # Only if using reconfirmable
     add_index :users, :confirmation_token, unique: true
-    # User.reset_column_information # Need for some types of updates, but not for update_all.
-    # To avoid a short time window between running the migration and updating all existing
-    # users as confirmed, do the following
-    User.update_all confirmed_at: DateTime.now
+
+    # Update all existing users as confirmed
+    execute <<-SQL.squish
+      UPDATE users SET confirmed_at = NOW()
+    SQL
   end
 
   def down
