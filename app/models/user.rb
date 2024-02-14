aeb92f73826af3c8af06bd67e59bacd2afd47cec
yusufyusie <yesuf023@gmail.com>
2024-02-14 09:17:13 +0300

    Add :confirmable to the list of Devise modules in your User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
+
+  validates :name, presence: true, length: { maximum: 35 }
 end
