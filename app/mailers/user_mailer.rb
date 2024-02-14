163ad21ddf6322dedf50916796c0e8fbce3cc038
yusufyusie <yesuf023@gmail.com>
2024-02-14 13:31:14 +0300

    Update user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,7 @@
-class UserMailer < Devise::Mailer
-  default from: 'no-reply@yourdomain.com'
-  layout 'mailer'
-
+class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
-    mail(to: @user.email, subject: 'Welcome to our awesome platform')
+    @url  = new_user_session_url
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 end
