216e809e2c7ab50a84ff4319e40b047d009a509b
yusufyusie <yesuf023@gmail.com>
2024-02-14 20:55:36 +0300

    Update migration for confirm token

--- db/migrate/20240214061512_add_confirmable_to_devise.rb
+++ db/migrate/20240214061512_add_confirmable_to_devise.rb
@@ -1,10 +1,8 @@
 class AddConfirmableToDevise < ActiveRecord::Migration[7.1]
   def up
-    add_column :users, :confirmation_token, :string
     add_column :users, :confirmed_at, :datetime
     add_column :users, :confirmation_sent_at, :datetime
     add_column :users, :unconfirmed_email, :string # Only if using reconfirmable
-    add_index :users, :confirmation_token, unique: true
 
     # Update all existing users as confirmed
     execute <<-SQL.squish
@@ -13,7 +11,7 @@ class AddConfirmableToDevise < ActiveRecord::Migration[7.1]
   end
 
   def down
-    remove_columns :users, :confirmation_token, :confirmed_at, :confirmation_sent_at
+    remove_columns :users, :confirmed_at, :confirmation_sent_at
     remove_columns :users, :unconfirmed_email # Only if using reconfirmable
   end
 end
