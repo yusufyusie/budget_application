c05871b0a210259013ada767a449f53f34f5ebf6
yusufyusie <yesuf023@gmail.com>
2024-02-15 08:56:24 +0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_15_054341) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_15_055405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,8 +19,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_15_054341) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,5 +39,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_15_054341) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "groups", "users", on_delete: :cascade
 end
