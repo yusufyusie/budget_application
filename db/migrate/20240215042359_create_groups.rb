ed33d1e43ff31b9888ae2dc38fe8db93782532b2
yusufyusie <yesuf023@gmail.com>
2024-02-15 07:53:08 +0300

    Update group migration and schema

--- db/migrate/20240215042359_create_groups.rb
+++ db/migrate/20240215042359_create_groups.rb
@@ -3,7 +3,6 @@ class CreateGroups < ActiveRecord::Migration[7.1]
     create_table :groups do |t|
       t.string :name
       t.string :icon
-      t.bigint :user_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_15_042359) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_15_045024) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.string "icon"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +41,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_15_042359) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "groups", "users", on_delete: :cascade
 end
